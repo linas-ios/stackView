f5d2b0d7d962ffebffe47d9ed2716808db8bd3fd
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-10-21 01:13:12 +0200

    Initial Commit

---
+++ stackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackView
+//
+//  Created by Linas Nutautas on 21/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
